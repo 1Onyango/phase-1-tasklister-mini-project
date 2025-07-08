38b39e001e33ea637039600e06add12deb73d52f
Onyango Ouma <ico.ouma@gmail.com>
2025-07-09 02:53:41 +0300

    phase-1-tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const input = document.querySelector("#new-task-description");
+  const taskList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const li = document.createElement("li");
+    li.textContent = input.value + " ";
+
+    const btn = document.createElement("button");
+    btn.textContent = "Delete";
+    btn.onclick = () => li.remove();
+
+    li.appendChild(btn);
+    taskList.appendChild(li);
+
+    form.reset();
+  });
+
 });
